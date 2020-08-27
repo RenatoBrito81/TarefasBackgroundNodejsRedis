5cc8d18c882347b21872f6519661280f16a5feeb
Renato Brito <rb.renatobrito@gmail.com>
2020-08-26 22:00:02 -0300

    Inclusao do Bull-Board para visualizar o status das filas via browser.

--- src/server.js
+++ src/server.js
@@ -2,15 +2,23 @@
 import "dotenv/config";
 import express from "express";
 import userController from "./app/controller/userController";
+import bullBoard from "bull-board";
+import queue from "../src/app/lib/queue";
 
 //Instância o servidor
 const app = express();
+
+//Instância bull-board
+bullBoard.setQueues(queue.queues.map(queue => queue.bull));
 
 //Configura o servidor para aceitar requisições no formato json
 app.use(express.json());
 
 //Configura a rota de POST
 app.post("/users", userController.store);
+
+//Configura a rota para utilizar o bull-board
+app.use("/admin/queues", bullBoard.UI);
 
 //Executa o servidor
 app.listen(process.env.SERVER_PORT, () => {
